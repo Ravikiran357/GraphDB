a802cf41bb9276cb12d2056b9bdd49166cc133e6
Ravi <ravikiran.tangirala@asu.edu>
2017-04-17 00:10:12 -0700

    Handled task 9a with label filters

--- minjava/javaminibase/src/iterator/SmjEdge.java
+++ minjava/javaminibase/src/iterator/SmjEdge.java
@@ -1,21 +1,13 @@
 package iterator;
 
-import java.io.IOException;
-
-import edgeheap.EScan;
-import edgeheap.Edge;
-import edgeheap.InvalidTupleSizeException;
 import global.AttrOperator;
 import global.AttrType;
-import global.EID;
-import global.SystemDefs;
 import global.TupleOrder;
-import heap.FieldNumberOutOfBoundException;
 import heap.Heapfile;
 import heap.Tuple;
 
 public class SmjEdge {
-	public static Heapfile joinHeapfile;
+	public Heapfile joinHeapfile;
 	private void Query1_CondExpr(CondExpr[] expr) {
 
 		expr[0].next = null;
@@ -28,107 +20,194 @@ public class SmjEdge {
 		expr[1] = null;
 	}
 	
-	private void printEdgesInHeap() throws InvalidTupleSizeException, IOException, 
-	FieldNumberOutOfBoundException {
-		EScan eScan = new EScan(SystemDefs.JavabaseDB.edgeHeapfile);
-	    EID eid = new EID();
-	    System.out.println("Printing edge data using edge heap file");
-	    Edge edge = eScan.getNext(eid);
-	    while(edge != null) {
-	    	// Print edge details
-	    	edge.print();
-	    	edge = eScan.getNext(eid);
-	    }
-	    eScan.closescan();
+	public SmjEdge() throws UnknowAttrType, LowMemException, JoinsException, Exception {
 	}
 	
-//	public String returnJoinedFile(){
-//		return "joinheapfile";
-//	}
+	private AttrType[] setAttrs(int joinRelationsType) {
+		AttrType[] attrs = null;
+		int numOfAttrs = (joinRelationsType == 0 ? 6 : 12);
+		attrs = new AttrType[numOfAttrs];
+		switch(joinRelationsType) {
+		case 0:
+			attrs[0] = new AttrType(AttrType.attrString);
+			attrs[1] = new AttrType(AttrType.attrInteger); //source pg no.
+			attrs[2] = new AttrType(AttrType.attrInteger);//source slot no.
+			attrs[3] = new AttrType(AttrType.attrInteger);//dest pg no.
+			attrs[4] = new AttrType(AttrType.attrInteger);//dest slot no.
+			attrs[5] = new AttrType(AttrType.attrInteger);
+			break;
+		case 1:
+			attrs[0] = new AttrType(AttrType.attrString);
+			attrs[1] = new AttrType(AttrType.attrInteger); //source pg no.
+			attrs[2] = new AttrType(AttrType.attrInteger);//source slot no.
+			attrs[3] = new AttrType(AttrType.attrInteger);//dest pg no.
+			attrs[4] = new AttrType(AttrType.attrInteger);//dest slot no.
+			attrs[5] = new AttrType(AttrType.attrInteger);
+			attrs[6] = new AttrType(AttrType.attrString);
+			attrs[7] = new AttrType(AttrType.attrInteger); //source pg no.
+			attrs[8] = new AttrType(AttrType.attrInteger);//source slot no.
+			attrs[9] = new AttrType(AttrType.attrInteger);//dest pg no.
+			attrs[10] = new AttrType(AttrType.attrInteger);//dest slot no.
+			attrs[11] = new AttrType(AttrType.attrInteger);
+			break;
+		}
+		
+		return attrs;
+	}
 
-	
-	public SmjEdge(String filename1, String filename2) throws UnknowAttrType, LowMemException, JoinsException, Exception {
-		printEdgesInHeap();
-		joinHeapfile = new Heapfile("joinheapfile");
-		AttrType[] attrs = new AttrType[6];
-        attrs[0] = new AttrType(AttrType.attrString);
-        attrs[1] = new AttrType(AttrType.attrInteger); //source pg no.
-        attrs[2] = new AttrType(AttrType.attrInteger);//source slot no.
-        attrs[3] = new AttrType(AttrType.attrInteger);//dest pg no.
-        attrs[4] = new AttrType(AttrType.attrInteger);//dest slot no.
-        attrs[5] = new AttrType(AttrType.attrInteger);
-		short[] attrSize = new short[1];
-		attrSize[0] = 44;
-		RelSpec rel_out = new RelSpec(RelSpec.outer);	
+	private short[] setAttrSizes(int joinRelationsType) {
+		short[] attrSize = null;
+		switch(joinRelationsType) {
+		case 0:
+			attrSize = new short[1];
+			attrSize[0] = 44;
+			break;
+		case 1:
+			attrSize = new short[2];
+			attrSize[0] = 44;
+			attrSize[1] = 44;
+			break;
+		}
+		return attrSize;
+	}
+
+	private FldSpec[] setFieldSpecs(int joinRelationsType, boolean is_res) {
+		FldSpec[] rel_projList = null;
 		RelSpec rel_in = new  RelSpec(RelSpec.innerRel);
+		RelSpec rel_type = new RelSpec(RelSpec.outer);
+		int fieldSpecSize = 0;
+		if (joinRelationsType == 0) {
+			if (is_res)
+				fieldSpecSize = 12;
+			else
+				fieldSpecSize = 6;
+		}
+		else if (joinRelationsType == 1) {
+			if (is_res)
+				fieldSpecSize = 18;
+			else
+				fieldSpecSize = 12;
+		}
+		rel_projList = new FldSpec[fieldSpecSize];
+		switch(joinRelationsType) {
+		case 0:
+			rel_projList[0] = new FldSpec(rel_type, 1);
+			rel_projList[1] = new FldSpec(rel_type, 2);
+			rel_projList[2] = new FldSpec(rel_type, 3);
+			rel_projList[3] = new FldSpec(rel_type, 4);
+			rel_projList[4] = new FldSpec(rel_type, 5);
+			rel_projList[5] = new FldSpec(rel_type, 6);
+			if (is_res) {
+				rel_projList[6] = new FldSpec(rel_in, 1);
+				rel_projList[7] = new FldSpec(rel_in, 2);
+				rel_projList[8] = new FldSpec(rel_in, 3);
+				rel_projList[9] = new FldSpec(rel_in, 4);
+				rel_projList[10] = new FldSpec(rel_in, 5);
+				rel_projList[11] = new FldSpec(rel_in, 6);
+			}
+			break;
+		case 1:
+			rel_projList[0] = new FldSpec(rel_type, 1);
+			rel_projList[1] = new FldSpec(rel_type, 2);
+			rel_projList[2] = new FldSpec(rel_type, 3);
+			rel_projList[3] = new FldSpec(rel_type, 4);
+			rel_projList[4] = new FldSpec(rel_type, 5);
+			rel_projList[5] = new FldSpec(rel_type, 6);
+			rel_projList[6] = new FldSpec(rel_type, 7);
+			rel_projList[7] = new FldSpec(rel_type, 8);
+			rel_projList[8] = new FldSpec(rel_type, 9);
+			rel_projList[9] = new FldSpec(rel_type, 10);
+			rel_projList[10] = new FldSpec(rel_type, 11);
+			rel_projList[11] = new FldSpec(rel_type, 12);
+			if (is_res) {
+				rel_projList[12] = new FldSpec(rel_in, 1);
+				rel_projList[13] = new FldSpec(rel_in, 2);
+				rel_projList[14] = new FldSpec(rel_in, 3);
+				rel_projList[15] = new FldSpec(rel_in, 4);
+				rel_projList[16] = new FldSpec(rel_in, 5);
+				rel_projList[17] = new FldSpec(rel_in, 6);
+			}
+			break;
+		}
+		return rel_projList;
+	}
 		
-		FldSpec[] out_projlist = new FldSpec[6];
-		out_projlist[0] = new FldSpec(rel_out, 1);
-		out_projlist[1] = new FldSpec(rel_out, 2);
-		out_projlist[2] = new FldSpec(rel_out, 3);
-		out_projlist[3] = new FldSpec(rel_out, 4);
-		out_projlist[4] = new FldSpec(rel_out, 5);
-		out_projlist[5] = new FldSpec(rel_out, 6);
-		
-		FileScan out_fscan = new FileScan(filename1, attrs, attrSize, (short) 6, 6, out_projlist, null);
-		FldSpec[] in_projlist = new FldSpec[6];
-		in_projlist[0] = new FldSpec(rel_in, 1);
-		in_projlist[1] = new FldSpec(rel_in, 2);
-		in_projlist[2] = new FldSpec(rel_in, 3);
-		in_projlist[3] = new FldSpec(rel_in, 4);
-		in_projlist[4] = new FldSpec(rel_in, 5);
-		in_projlist[5] = new FldSpec(rel_in, 6);
-		FileScan in_fscan = new FileScan(filename2, attrs, attrSize, (short) 6, 6, out_projlist, null);
-
-		
-		FldSpec[] projlist = new FldSpec[6];
-		projlist[0] = new FldSpec(rel_out, 1);
-		projlist[1] = new FldSpec(rel_out, 4);
-		projlist[2] = new FldSpec(rel_out, 5);
-		projlist[3] = new FldSpec(rel_in, 1);
-		projlist[4] = new FldSpec(rel_in, 2);
-		projlist[5] = new FldSpec(rel_in, 3);
-		TupleOrder ascending = new TupleOrder(TupleOrder.Ascending);
+	//
+	//JoinTRelationsType = 0 for outer = edge and inner = edge ; out_col1 = 12
+	//JoinTRelationsType = 1 for outer = out_col1(12) and inner = edge ; out_col2 = 18
+	//
+	public void joinOperation(String filename1, String filename2, String res_filename, int joinRelationsType, boolean asc_order) throws 
+	UnknowAttrType, LowMemException, JoinsException, Exception {
+		AttrType[] attrs = setAttrs(joinRelationsType);
+		short[] attrSize = setAttrSizes(joinRelationsType);
+		FldSpec[] projlist = setFieldSpecs(joinRelationsType, false);
+		FldSpec[] res_projlist = setFieldSpecs(joinRelationsType, true);
+		int rnumCols = 0;
+		int snumCols = 0;
+		int resNumCols = 0;
+		int r_col = 0;
+		int s_col = 3;
+		if (joinRelationsType == 0) {
+			rnumCols = 6;
+			snumCols = 6;
+			resNumCols = 12;
+			r_col = 5;
+		}
+		else {
+			rnumCols = 12;
+			snumCols = 6;
+			resNumCols = 18;
+			r_col = 11;
+		}
+		FileScan r_fscan = new FileScan(filename1, attrs, attrSize, (short) rnumCols, rnumCols, projlist, null);
+		FileScan s_fscan  = new FileScan(filename2, attrs, attrSize, (short) snumCols, snumCols, projlist, null);
+		TupleOrder order = new TupleOrder((asc_order ? TupleOrder.Ascending : TupleOrder.Descending));
 		SortMerge sm = null;
+		joinHeapfile = new Heapfile(res_filename);
 		
-		CondExpr[] outFilter = new CondExpr[3];
-		outFilter[0] = new CondExpr();
-		outFilter[1] = new CondExpr();
-		outFilter[2] = new CondExpr();
-		Query1_CondExpr(outFilter);
+//		CondExpr[] outFilter = new CondExpr[3];
+//		outFilter[0] = new CondExpr();
+//		outFilter[1] = new CondExpr();
+//		outFilter[2] = new CondExpr();
+//		Query1_CondExpr(outFilter);
+		int numBuf = 0;
+		if (joinRelationsType == 0){
+			numBuf = 40;
+		} else{
+			numBuf = 45;
+		}
 		
 		try {
 			sm = new SortMerge(
-					attrs, 6, attrSize, attrs, 6, attrSize, 
-					5, 4, 3, 4,
-					25, out_fscan, in_fscan, false, false, ascending,
-					null, projlist, 6, -1.0, null);
+					attrs, rnumCols, attrSize, attrs, snumCols, attrSize, 
+					r_col, 4, s_col, 4,
+					numBuf, r_fscan, s_fscan, false, false, order,
+					null, res_projlist, resNumCols, -1.0, null);
 		} catch (Exception e) {
 			System.err.println("*** join error in SortMerge constructor ***");
 			System.err.println("" + e);
 			e.printStackTrace();
 		}
 		Tuple t = sm.get_next();
-		int counter =0;
+		int counter = 0;
 		while (t != null) {
 			try {
 				System.out.println("Counter = "+ counter++);
-				String outval = t.getStrFld(1);
-				int outval1 = t.getIntFld(2);
-				int outval2 = t.getIntFld(3);
-				String outval3 = t.getStrFld(4);
-				int outval4 = t.getIntFld(5);
-				int outval5 = t.getIntFld(6);
-				System.out.println(outval + " " + outval1 + " " + outval2 + " " + outval3 + " " + outval4 + " " + outval5);
+				for (int i = 1; i <= resNumCols; i++) {
+					if (i == 1 || i == 7 || i == 13)
+						System.out.print(t.getStrFld(i) + " ");
+					else
+						System.out.print(t.getIntFld(i) + " ");
+					if (i % 6 == 0)
+						System.out.print(" | ");
+				}
 				System.out.println("!!!!!!");
+				joinHeapfile.insertRecord(t.getTupleByteArray());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			joinHeapfile.insertRecord(t.getTupleByteArray());
 			t = sm.get_next();
-			
-			//returnJoinedFile();
 		}
+		sm.close();
 	}
-	
 }
--- minjava/javaminibase/src/tests/TriangleQuery.java
+++ minjava/javaminibase/src/tests/TriangleQuery.java
@@ -21,7 +21,6 @@ import heap.InvalidTypeException;
 import heap.Scan;
 import heap.SpaceNotAvailableException;
 import heap.Tuple;
-import iterator.FileScan;
 import iterator.JoinsException;
 import iterator.LowMemException;
 import iterator.SmjEdge;
@@ -39,140 +38,75 @@ public class TriangleQuery {
 		label3 = "3_1";
 		
 	}
-	private Tuple setHdrFilter1(Tuple t) throws InvalidTypeException, InvalidTupleSizeException, IOException {
+
+	private Tuple setHdr(Tuple t) throws InvalidTypeException, InvalidTupleSizeException, IOException {
 		AttrType[] attrs = new AttrType[6];
         short[] str_sizes = new short[1];
         attrs[0] = new AttrType(AttrType.attrString);
         attrs[1] = new AttrType(AttrType.attrInteger); //source pg no.
-        attrs[2] = new AttrType(AttrType.attrInteger);//source slot no.
-        attrs[3] = new AttrType(AttrType.attrInteger);//dest pg no.
-        attrs[4] = new AttrType(AttrType.attrInteger);//dest slot no.
+        attrs[2] = new AttrType(AttrType.attrInteger); //source slot no.
+        attrs[3] = new AttrType(AttrType.attrInteger); //dest pg no.
+        attrs[4] = new AttrType(AttrType.attrInteger); //dest slot no.
         attrs[5] = new AttrType(AttrType.attrInteger);
         str_sizes[0] = (short)44;
         t.setHdr((short)6, attrs, str_sizes);
         return t;
 	}
 	
-	public void filterTupleLabels(EdgeHeapfile hf,  String label, 
-			String outheapfile) throws HFException, HFBufMgrException, HFDiskMgrException, 
-			IOException, InvalidTupleSizeException, InvalidSlotNumberException, SpaceNotAvailableException, 
-			FieldNumberOutOfBoundException, edgeheap.InvalidTupleSizeException, InvalidTypeException {
-		//Heapfile hf = new Heapfile(heapfilename);
+	public void filterTupleLabels(EdgeHeapfile hf,  String label, String outheapfile) throws 
+			HFException, HFBufMgrException, HFDiskMgrException, IOException, InvalidTupleSizeException,
+			InvalidSlotNumberException, SpaceNotAvailableException,	FieldNumberOutOfBoundException,
+			edgeheap.InvalidTupleSizeException, InvalidTypeException {
+		Heapfile outhf = new Heapfile(outheapfile);
 		EScan fscan = new EScan(hf);
-		Heapfile outhf = new Heapfile(outheapfile);
-		
-		
 		EID rid = new EID();
-        boolean done = true;
-        while(done){
-            Edge e  = fscan.getNext(rid);
-            if(e == null){
-                done = false;
-                fscan.closescan();
-                break;
-            }
-            Tuple t = new Tuple(e.getTupleByteArray(), 0, e.getLength());
-            t = setHdrFilter1(t);
+		Edge edge = fscan.getNext(rid);
+        while(edge != null){
+            Tuple t = new Tuple(edge.getTupleByteArray(), 0, edge.getLength());
+            t = setHdr(t);
             if(t.getStrFld(1).equals(label)){
-            	//Add to the new heapfile
-//                Tuple tuple = new Tuple(t.getEdgeByteArray(),0,t.getLength());
-//                tuple.setHdr((short)6, attrs, str_sizes);
+            	//Add tuples to the new heapfile
                 outhf.insertRecord(t.getTupleByteArray());
             }
+            edge = fscan.getNext(rid);
         }
- 
-		
+        fscan.closescan();
 	}
-	
-	
-	public void filterTupleLabels23(Heapfile hf,  String label, 
-			String outheapfile) throws HFException, HFBufMgrException, HFDiskMgrException, 
-			IOException, InvalidTupleSizeException, InvalidSlotNumberException, SpaceNotAvailableException, 
-			FieldNumberOutOfBoundException, edgeheap.InvalidTupleSizeException, InvalidTypeException {
-		//Heapfile hf = new Heapfile(heapfilename);
-		Scan fscan = new Scan(hf);
-		Heapfile outhf = new Heapfile(outheapfile);
-		
-		
-		RID rid = new RID();
-        boolean done = true;
-        while(done){
-            Tuple t  = fscan.getNext(rid);
-            if(t == null){
-                done = false;
-                fscan.closescan();
-                break;
-            }
-            t = setHdrFilter1(t);
-            if(t.getStrFld(4).equals(label)){
-            	//Add to the new heapfile
-//                Tuple tuple = new Tuple(t.getEdgeByteArray(),0,t.getLength());
-//                tuple.setHdr((short)6, attrs, str_sizes);
-                outhf.insertRecord(t.getTupleByteArray());
-            }
-        }
- 
-		
-	}
-	
-	
-	public void printTuplesInRelation(String heapfilename) throws FieldNumberOutOfBoundException, IOException,
-	InvalidTupleSizeException, HFException, HFBufMgrException, HFDiskMgrException, InvalidTypeException{
+
+	public void printTuplesInRelation(String heapfilename) throws FieldNumberOutOfBoundException, 
+		IOException, InvalidTupleSizeException, HFException, HFBufMgrException, 
+		HFDiskMgrException, InvalidTypeException{
 		Heapfile hf = new Heapfile(heapfilename);
 		Scan fscan = new Scan(hf);
 		RID rid = new RID();
-        boolean done = true;
-        while(done){
-        	Tuple t  = fscan.getNext(rid);
-            if(t == null){
-                done = false;
-                fscan.closescan();
-                break;
-            }
-            t = setHdrFilter1(t);
+		Tuple t = fscan.getNext(rid);
+        while(t != null){
+            t = setHdr(t);
             System.out.println(t.getStrFld(1));
+            t = fscan.getNext(rid);
         }
+        fscan.closescan();
 	}
-	
-	
-	public void printTuplesInRelation2(Heapfile heapfilename) throws FieldNumberOutOfBoundException, IOException,
-	InvalidTupleSizeException, HFException, HFBufMgrException, HFDiskMgrException, InvalidTypeException{
-		//Heapfile hf = new Heapfile(heapfilename);
-		Scan fscan = new Scan(heapfilename);
-		RID rid = new RID();
-        boolean done = true;
-        while(done){
-        	Tuple t  = fscan.getNext(rid);
-            if(t == null){
-                done = false;
-                fscan.closescan();
-                break;
-            }
-            t = setHdrFilter1(t);
-            System.out.println(t.getStrFld(1));
-        }
-	}
-	
-	
+
 	public void startTriangleQuery() throws UnknowAttrType, LowMemException, JoinsException, Exception{
-		//From the edge relation filter label1 from outer
 		EdgeHeapfile hf = SystemDefs.JavabaseDB.edgeHeapfile;
+		int joinOperationType = 0;
 
-		String outheapfile = "filterlabels1";
-		String inheapfile = "filterlabels2";
-		filterTupleLabels(hf, label1, outheapfile);
-		filterTupleLabels(hf, label2, inheapfile);
-		//printTuplesInRelation(outheapfile);
-		String joinheapfile = "joinheapfile";
+		//From the edge relation filter label1 from R relation and label2 from S relation
+		String rheapfile = "filterlabels1";
+		String sheapfile = "filterlabels2";
+		filterTupleLabels(hf, label1, rheapfile);
+		filterTupleLabels(hf, label2, sheapfile);
+		String joinheapfile1 = "joinheapfile1";
+		SmjEdge smj1 = new SmjEdge();
+		smj1.joinOperation(rheapfile, sheapfile, joinheapfile1, joinOperationType, true);
 		
-		SmjEdge smj = new SmjEdge(outheapfile, inheapfile);
-		printTuplesInRelation2(smj.joinHeapfile);
-
-		//Filter inner based on label 3 
-		filterTupleLabels(hf, label3, inheapfile);
-		
-		//Pass the already joined heapfile and the filtered file as input to smj
-		
-		
+		//Pass the already joined heapfile and the file filtered on label3 as input to smj
+		joinOperationType = 1;
+		String sheapfile_s = "filterlabels3";
+		filterTupleLabels(hf, label3, sheapfile_s);
+		String joinheapfile2 = "joinheapfile2";
+		SmjEdge smj2 = new SmjEdge();
+		smj2.joinOperation(joinheapfile1, sheapfile_s, joinheapfile2, joinOperationType, true);
 	}
 }
