e5040baf214344758df22a6820688672bbd43b79
Ravi <ravikiran.tangirala@asu.edu>
2017-04-25 19:33:17 -0700

    Added some prints

--- minjava/javaminibase/src/tests/PathQuery2.java
+++ minjava/javaminibase/src/tests/PathQuery2.java
@@ -3,6 +3,8 @@ package tests;
 
 import java.io.IOException;
 
+import diskmgr.PCounter;
+
 import edgeheap.Edge;
 import zIndex.DescriptorKey;
 import zIndex.ZTreeFile;
@@ -58,15 +60,16 @@ public class PathQuery2 {
 	private String [] edge_path;
 	private int no_of_edges;
 
-	PathQuery2(String path, boolean is_desc) throws nodeheap.InvalidTupleSizeException,
-		IOException, KeyNotMatchException, IteratorException, ConstructPageException, 
-		PinPageException, UnpinPageException, InvalidFrameNumberException, ReplacerException, 
-		PageUnpinnedException, HashEntryNotFoundException, ScanIteratorException {
+	PathQuery2(String path) throws FieldNumberOutOfBoundException, 
+		nodeheap.InvalidSlotNumberException, nodeheap.HFException, 
+		nodeheap.HFDiskMgrException, nodeheap.HFBufMgrException, Exception {
 		no_of_edges = path.length() - path.replace("/", "").length();
 		edge_path = new String[no_of_edges + 1];
 		edge_path = path.split("/");
 		BTFileScan iscan;
-		if (is_desc) {
+		NID nid = null;
+		// getting the label using the descriptor
+		if (edge_path[0].contains(",")) {
 			String[] node_desc = new String[5];
 			node_desc = edge_path[0].split(",");
 			ZTreeFile node_index = SystemDefs.JavabaseDB.nodeDescriptorIndexFile;
@@ -75,25 +78,23 @@ public class PathQuery2 {
 					Integer.parseInt(node_desc[2]),Integer.parseInt(node_desc[3]), 
 					Integer.parseInt(node_desc[4]));
 			iscan = node_index.new_scan(new DescriptorKey(node_key), new DescriptorKey(node_key));
-		} else {
-			BTreeFile node_index = SystemDefs.JavabaseDB.nodeLabelIndexFile;
-			iscan = node_index.new_scan(new StringKey(edge_path[0]), new StringKey(edge_path[0]));
+			KeyDataEntry entry = iscan.get_next();
+			if (entry != null) {
+				// Get NID
+				LeafData leafData = (LeafData) entry.data;
+				nid = new NID();
+				nid.copyRid(leafData.getData());
+			}
+			iscan.DestroyBTreeFileScan();
+			edge_path[0] = SystemDefs.JavabaseDB.nodeHeapfile.getNode(nid).getLabel();
 		}
-		KeyDataEntry entry = iscan.get_next();
-		while (entry != null) {
-			// Get NID
-//			LeafData leafData = (LeafData) entry.data;
-//			first_nid = new NID();
-//			first_nid.copyRid(leafData.getData());
-			break;
-		}
-		iscan.DestroyBTreeFileScan();	
 	}
 	
 	private Edge getNextindexFilterSource(BTFileScan iscan, String edgeLabel) throws edgeheap.InvalidSlotNumberException, edgeheap.InvalidTupleSizeException, edgeheap.HFException, edgeheap.HFDiskMgrException, edgeheap.HFBufMgrException, Exception{
 		KeyDataEntry keyData = iscan.get_next();
 		if (keyData == null)
 			return null;
+		System.out.println("Selection on index key (label): " + edgeLabel);
 		LeafData edgeLeaf =  (LeafData)keyData.data;
 		EID edgeId = new EID();
 		edgeId.copyRid(edgeLeaf.getData());
@@ -112,6 +113,7 @@ public class PathQuery2 {
 		KeyDataEntry keyData = iscan.get_next();
 		if (keyData == null)
 			return null;
+		System.out.println("Selection on index key (weight): " + edgeWeight);
 		LeafData edgeLeaf =  (LeafData)keyData.data;
 		EID edgeId = new EID();
 		edgeId.copyRid(edgeLeaf.getData());
@@ -125,8 +127,7 @@ public class PathQuery2 {
 			return getNextindexFilterWeight(iscan, edgeWeight);
 		}
 	}
-	
-	
+
 	public void sortLabels(String nodelabelheapfile, String sortedResFile) 
 			throws JoinsException, IndexException, InvalidTupleSizeException, InvalidTypeException, 
 			PageNotReadException, PredEvalException, LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception{
@@ -213,38 +214,35 @@ public class PathQuery2 {
 		resSort.close();
 		dupeli.close();
 	}
-	
-	
-	
+
 	public void printTuplesInRelation(String heapfilename) throws FieldNumberOutOfBoundException, 
-	IOException, InvalidTupleSizeException, HFException, HFBufMgrException, 
-	HFDiskMgrException, InvalidTypeException{
-	
-	int count = 0;
-	AttrType[] attrs = new AttrType[2];
-	attrs[0] = new AttrType(AttrType.attrString);
-	attrs[1] = new AttrType(AttrType.attrString);
-	
-	
-	short[] str_sizes = new short[2];
-	str_sizes[0] = (short)44;
-	str_sizes[1] = (short)44;
-	
-	
-	Heapfile hf = new Heapfile(heapfilename);
-	Scan fscan = new Scan(hf);
-	RID rid = new RID();
-	Tuple t = fscan.getNext(rid);
-    while(t != null){
-    	count++;
-		t.setHdr((short)2, attrs, str_sizes);
-        System.out.println(t.getStrFld(1) + " " + t.getStrFld(2));
-        t = fscan.getNext(rid);
-    }
-    System.out.println("Total count = "+ count);
-    fscan.closescan();
-}
-	
+		IOException, InvalidTupleSizeException, HFException, HFBufMgrException, 
+		HFDiskMgrException, InvalidTypeException{
+		
+		int count = 0;
+		AttrType[] attrs = new AttrType[2];
+		attrs[0] = new AttrType(AttrType.attrString);
+		attrs[1] = new AttrType(AttrType.attrString);
+		
+		
+		short[] str_sizes = new short[2];
+		str_sizes[0] = (short)44;
+		str_sizes[1] = (short)44;
+		
+		
+		Heapfile hf = new Heapfile(heapfilename);
+		Scan fscan = new Scan(hf);
+		RID rid = new RID();
+		Tuple t = fscan.getNext(rid);
+	    while(t != null){
+	    	count++;
+			t.setHdr((short)2, attrs, str_sizes);
+	        System.out.println(t.getStrFld(1) + " " + t.getStrFld(2));
+	        t = fscan.getNext(rid);
+	    }
+	    System.out.println("Total count = "+ count);
+	    fscan.closescan();
+	}	
 	
 	/*
 	 * input parameters
@@ -267,37 +265,24 @@ public class PathQuery2 {
 			e = getNextindexFilterWeight(iscan, weight);
 		}
 		
-//		while(e != null) {
-//			e.print();
-//			e = getNextindexFilterSource(iscan, edge_path[edgeLabelIndex]);
-//			}
 		while (e != null) {
 			// if all the joins are performed print the tail
 			if (edgeLabelIndex == no_of_edges) {
 				AttrType[] attrs = new AttrType[2];
 				attrs[0] = new AttrType(AttrType.attrString);
 				attrs[1] = new AttrType(AttrType.attrString);
-				
 				short[] str_sizes = new short[2];
-				
-				Tuple t = new Tuple();
-				
 				str_sizes[0] = (short)44;
 				str_sizes[1] = (short)44;
-				
+				Tuple t = new Tuple();
 				t.setHdr((short)2, attrs, str_sizes);
-				
-				//System.out.println("!!!!!!!");
 				String destnode = SystemDefs.JavabaseDB.nodeHeapfile.getNode(e.getDestination()).getStrFld(1);
 				t.setStrFld(1, firstLabel);
 				t.setStrFld(2, destnode);
-				hf.insertRecord(t.getTupleByteArray());
-				//e.print();				
+				hf.insertRecord(t.getTupleByteArray());			
 			} else {
-				//e.print();
 				String sourceLabel = SystemDefs.JavabaseDB.nodeHeapfile.getNode(
 						e.getDestination()).getLabel();//e's destination which will be source to inner guy
-				//edgeLabelIndex++;
 				NestedLoopJoin(sourceLabel, edgeLabelIndex+1, outhf, firstLabel);
 			}
 			if (edge_path[edgeLabelIndex].startsWith("L")){
@@ -307,7 +292,7 @@ public class PathQuery2 {
 				String weight = edge_path[edgeLabelIndex].substring(1).trim();
 				e = getNextindexFilterWeight(iscan, weight);
 			}
-		}	
+		}
 		iscan.DestroyBTreeFileScan();
 	}
 	
@@ -316,20 +301,31 @@ public class PathQuery2 {
 		String outhf = "outputheapfile";
 		String resSorthf = "sortheapfile";
 		String resDistincthf = "distinctheapfile";
+		SystemDefs.JavabaseDB.resetPageCounter();
 		NestedLoopJoin(edge_path[0], 1, outhf, edge_path[0]);
+		System.out.println("No of pages read: " + PCounter.rcounter + "\nNo of pages written: " + PCounter.wcounter);
+		
+		SystemDefs.JavabaseDB.resetPageCounter();
 		if(query.equals("a")){
+			System.out.println("------------------");
+			System.out.println("------- Task 7a: PQ2 - Insertion order -------");
 			printTuplesInRelation(outhf);
 		}
 		
 		if(query.equals("b")){
+			System.out.println("------------------");
+			System.out.println("------- Task 7b: PQ2 - Sorted order -------");
 			sortLabels(outhf, resSorthf);
 			printTuplesInRelation(resSorthf);
 		}
 		
 		if(query.equals("c")){
+			System.out.println("------------------");
+			System.out.println("------- Task 7c: PQ2 - Distinct nodes -------");
 			distinctLabels(outhf, resDistincthf);
 			printTuplesInRelation(resDistincthf);
 		}
+		System.out.println("No of pages read: " + PCounter.rcounter + "\nNo of pages written: " + PCounter.wcounter);
 		cleanup(outhf, resSorthf, resDistincthf);
 	}
 	
@@ -342,7 +338,5 @@ public class PathQuery2 {
 		nhf.deleteFile();
 		srf.deleteFile();
 		drf.deleteFile();
-		
 	}
-	
 }
